37a4f7a31d3063b899e7ff6564b2f317637514fb
Amity AppLabs <amityapplabs@rajats-macbook-pro.local>
2018-04-03 14:40:01 +0530

    Initial Commit

---
+++ Amity Incubator App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amity Incubator App
+//
+//  Created by Amity AppLabs on 03/04/18.
+//  Copyright © 2018 Amity Innovation Incubator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

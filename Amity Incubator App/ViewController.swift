dde74eb2b54fad830ba6b0c200548791d0f914a0
rajat khanna <legalimpurity@gmail.com>
2018-04-26 15:50:00 +0530

    first session push.

--- Amity Incubator App/ViewController.swift
+++ Amity Incubator App/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theAnimeshLabel: UILabel!
+
+    var incrementCounter = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func changeTextFunction(_ sender: Any) {
+        incrementCounter = incrementCounter + 1
+        theAnimeshLabel.text = "amity incubator "+String(incrementCounter)
+    }
+    
 }
 
